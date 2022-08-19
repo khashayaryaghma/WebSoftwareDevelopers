d5580f54a5839963fdad17244c5d0e507bf67b79
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-08-19 22:31:04 +0430

    add pages to router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Home from "./pages/Home/Home";
+import Contact from "./pages/Contact/Contact";
+import About from "./pages/About/About";
+import NotFound from "./pages/NotFound/NotFound";
 function App() {
   return (
       <div className="App">
           <Router>
               <Routes>
-                  <Route path="/"/>
-                  <Route path="contact"/>
-                  <Route path="about"/>
-                  <Route path="*"/>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="contact" element={<Contact/>}/>
+                  <Route path="about" element={<About/>}/>
+                  <Route path="*" element={<NotFound/>}/>
               </Routes>
           </Router>
       </div>
