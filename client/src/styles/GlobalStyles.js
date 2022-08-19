1bab65db4e85ca0104d2cfcbdbac05e744a7824c
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-08-20 00:59:47 +0430

    add font to global styles
    from google font

--- client/src/styles/GlobalStyles.js
+++ client/src/styles/GlobalStyles.js
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Barlow&display=swap');
   body {
     margin: 0;
     padding: 0;
+    font-family: 'Barlow', sans-serif;
   }
 `;
