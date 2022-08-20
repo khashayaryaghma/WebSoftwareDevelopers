62a705cc7e9f22df5cfb8e81328d3e45e2790641
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-08-21 00:26:34 +0430

    add globalStyle

--- client/src/styles/GlobalStyles.js
+++ client/src/styles/GlobalStyles.js
@@ -2,9 +2,12 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Barlow&display=swap');
-  body {
+  * {
     margin: 0;
     padding: 0;
     font-family: 'Barlow', sans-serif;
+  }
+  a{
+    text-decoration: none;
   }
 `;
