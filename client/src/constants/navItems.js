f6787b6215cc1a7966df8bfb3dde72a5dbd3aa12
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-08-21 00:07:15 +0430

    add navItems & activeStyle

---
+++ client/src/constants/navItems.js
@@ -0,0 +1,20 @@
+export const navItems =[
+    {
+        name:"Home",
+        route:"/"
+    },{
+        name:"About",
+        route:"/About"
+    },{
+        name:"contact",
+        route:"/contact"
+    }
+]
+
+export const activeStyle = {
+    backgroundColor: "gray",
+    borderRadius: "5px",
+    padding: "0 1rem ",
+    color: "white",
+    opacity:0.6
+};
