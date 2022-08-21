07f01362aeffa4bbdd49fc1b30229b025e00e7f7
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-08-21 21:25:39 +0430

    replace StyledImg in Header with reusable Image

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
-import { StyledHeader, StyledImg } from "./Header.styles";
+import { StyledHeader } from "./Header.styles";
 import Button from "../Button/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import Image from "../Image/Image";
 
-function Header({ bgColor, imgSrc , pd}) {
+function Header({ bgColor, imgSrc, pd }) {
     return (
         <StyledHeader bgColor={bgColor} pd={pd}>
             <Link to={"/"}>
-                <Image imgSrc={imgSrc} size="7rem" alt="Logo"/>
+                <Image imgSrc={imgSrc} size="7rem" alt="Logo" />
             </Link>
             <Button color="white" fSize="1.75rem" bgColor="inherit">
                 <FontAwesomeIcon icon={faBars} />
