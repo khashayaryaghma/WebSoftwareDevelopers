8cd28d834fe4dcb8ffdb034127eb8798616cbe6b
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-08-22 21:01:51 +0430

    add title and favicon with react-helmet

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import { Helmet } from "react-helmet";
+import logo from "./assets/images/logo.png";
 
-import { GlobalStyles } from './styles/GlobalStyles';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <GlobalStyles/>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Helmet>
+            <title>Web Development</title>
+            <link rel="icon" type="image/png" href={logo} />
+        </Helmet>
+        <GlobalStyles />
+        <App />
+    </React.StrictMode>
 );
