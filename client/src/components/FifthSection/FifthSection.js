7bc7796a25a52a17c47f563f4bd38548094a0032
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-08-23 21:17:30 +0430

    add empty FifthSection

---
+++ client/src/components/FifthSection/FifthSection.js
@@ -0,0 +1,10 @@
+import { StyledFifthSection } from "./FifthSection.styles"
+
+
+function FifthSection({bgColor}) {
+  return (
+    <StyledFifthSection bgColor={bgColor}>FifthSection</StyledFifthSection>
+  )
+}
+
+export default FifthSection
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import FirstSection from "../../components/FirstSection/FirstSection";
 import FourthSection from "../../components/FourthSection/FourthSection";
 import SecondSection from "../../components/SecondSection/SecondSection";
 import ThirdSection from "../../components/ThirdSection/ThirdSection";
+import FifthSection from "../../components/FifthSection/FifthSection";
 
 function Home() {
     return (
@@ -10,6 +11,7 @@ function Home() {
             <SecondSection />
             <ThirdSection bgColor="purple" />
             <FourthSection />
+            <FifthSection bgColor="purple" />
         </main>
     );
 }
