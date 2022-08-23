3082c4e5a8a490c2d9c9d2f4b92ef79f36f15b59
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-08-23 22:42:47 +0430

    add Grid

--- client/src/components/FifthSection/FifthSection.js
+++ client/src/components/FifthSection/FifthSection.js
@@ -1,10 +1,48 @@
-import { StyledFifthSection } from "./FifthSection.styles"
+import {
+    GridContainer,
+    StyledBox,
+    StyledDiv,
+    StyledFifthSection,
+} from "./FifthSection.styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faComputer,
+    faGlobe,
+    faRobot,
+    faWrench,
+} from "@fortawesome/free-solid-svg-icons";
 
-
-function FifthSection({bgColor}) {
-  return (
-    <StyledFifthSection bgColor={bgColor}>FifthSection</StyledFifthSection>
-  )
+function FifthSection({ bgColor }) {
+    return (
+        <StyledFifthSection bgColor={bgColor}>
+            <GridContainer>
+                <StyledBox>
+                    <StyledDiv>
+                        <FontAwesomeIcon icon={faComputer} />
+                    </StyledDiv>
+                    <p className="para">DEVELOPMENT</p>
+                </StyledBox>
+                <StyledBox>
+                    <StyledDiv>
+                        <FontAwesomeIcon icon={faGlobe} />
+                    </StyledDiv>
+                    <p className="para">5G NETWORK</p>
+                </StyledBox>
+                <StyledBox>
+                    <StyledDiv>
+                        <FontAwesomeIcon icon={faRobot} />
+                    </StyledDiv>
+                    <p className="para">AI PRODUCTS</p>
+                </StyledBox>
+                <StyledBox>
+                    <StyledDiv>
+                        <FontAwesomeIcon icon={faWrench} />
+                    </StyledDiv>
+                    <p className="para">AUTOMATION</p>
+                </StyledBox>
+            </GridContainer>
+        </StyledFifthSection>
+    );
 }
 
-export default FifthSection
+export default FifthSection;
--- client/src/components/FifthSection/FifthSection.styles.js
+++ client/src/components/FifthSection/FifthSection.styles.js
@@ -3,4 +3,29 @@ import styled from "styled-components";
 export const StyledFifthSection = styled.section`
     background-color: ${(props) =>
         props.bgColor === "purple" ? "#3C065E" : props.bgColor};
+    display: flex;
+`;
+
+export const GridContainer = styled.section`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1.2vmax;
+`;
+export const StyledBox = styled.div`
+    background-color: white;
+    width: 10rem;
+    height: 10rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StyledDiv = styled.div`
+    font-size: 2rem;
+    border-radius: 50%;
+    margin-bottom: 1rem;
+    padding: 0.5rem;
+    background-color: #29b7dd;
+    color: white;
 `;
