b7f29d03e8c9655e622a511b09de98c820912f5c
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-16 23:29:52 +0430

    add SixthSection

--- client/src/components/SixthSection/SixthSection.js
+++ client/src/components/SixthSection/SixthSection.js
@@ -1,8 +1,40 @@
+import { StyledSixthSection, StyledSixthArticle } from "./SixthSection.style"
+import Button from "../Button/Button";
+import Image from "../Image/Image";
+import sixthSection from "../../assets/images/sixthSection.jpg"
 
 function SixthSection() {
-  return (
-    <div>SixthSection</div>
-  )
+    return (
+        <StyledSixthSection>
+            <Image imgSrc={sixthSection} size="42vmax" />
+            <StyledSixthArticle>
+                <h1 className="heading" style={{ color: "#29B7DD" }}>
+                    IT solutions
+                </h1>
+                <p className="para">
+                    We collaborate with you to create new value through digital
+                    transformation. We have hundreds of specialists at your
+                    service.
+                </p>
+                <p className="para">
+                    Image from
+                    <a href="https://www.freepik.com/vectors/computer">
+                        {" "}
+                        Freepik
+                    </a>
+                </p>
+                <Button
+                    bgColor="#29B7DD"
+                    color="white"
+                    brRadius="30px"
+                    pd="0.5rem 3.2rem"
+                    fSize="1.5rem"
+                >
+                    L E A R N M O R E
+                </Button>
+            </StyledSixthArticle>
+        </StyledSixthSection>
+    )
 }
 
 export default SixthSection
---
+++ client/src/components/SixthSection/SixthSection.style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledSixthSection = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    min-height: 38rem;
+    padding-top: 1rem;
+`
+
+export const StyledSixthArticle = styled.article`
+    margin: 2rem;
+    width: 17rem;
+    min-height: 25rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: space-evenly;
+    text-align: right;
+`;
