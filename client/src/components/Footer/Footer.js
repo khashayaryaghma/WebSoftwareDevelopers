3f17293d7c0e835480f3bfe53c5e6db087ef2f41
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-17 23:08:20 +0430

    add article & contactus to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,42 @@
-import { StyledFooter } from "./Footer.style"
-
+import { StyledArticle, StyledContactUs, StyledFooter, StyledH3, StyledIconContainer, StyledPara } from "./Footer.style"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGuitar } from "@fortawesome/free-solid-svg-icons";
 
 function Footer({ bgColor }) {
-  return (
-    <StyledFooter bgColor={bgColor}>
-
-    </StyledFooter>
-  )
+    return (
+        <StyledFooter bgColor={bgColor}>
+            <StyledContactUs>
+                <div>
+                    <FontAwesomeIcon icon={faGuitar} />
+                    <StyledH3>OUR MAIN OFFICE</StyledH3>
+                    <StyledPara>SoHo 94 New York, NY 1001</StyledPara>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faGuitar} />
+                    <StyledH3>PHONE NUMBER</StyledH3>
+                    <StyledPara>234-9876-5400</StyledPara>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faGuitar} />
+                    <StyledH3>EMAIL</StyledH3>
+                    <StyledPara><a href="mailto:khashayaryaghma@gmail.com" style={{color:"#3C065E"}}>khashayaryaghma@gmail.com</a></StyledPara>
+                </div>
+            </StyledContactUs>
+            <StyledArticle>
+                <h2 className="heading">
+                    Get in touch
+                </h2>
+                <p className="para"><strong><em>We can ensure reliability, low cost fares and most important, with safety and comfort in mind.</em></strong></p>
+                <p className="para">Etiam sit amet convallis erat – class aptent taciti sociosqu ad litora torquent per conubia! Maecenas gravida lacus. Lorem etiam sit amet convallis erat.</p>
+                <StyledIconContainer>
+                    <FontAwesomeIcon icon={faGuitar} />
+                    <FontAwesomeIcon icon={faGuitar} />
+                    <FontAwesomeIcon icon={faGuitar} />
+                    <FontAwesomeIcon icon={faGuitar} />
+                </StyledIconContainer>
+            </StyledArticle>
+        </StyledFooter>
+    )
 }
 
 export default Footer
--- client/src/components/Footer/Footer.style.js
+++ client/src/components/Footer/Footer.style.js
@@ -3,4 +3,66 @@ import styled from "styled-components";
 export const StyledFooter = styled.footer`
     background-color: ${(props) =>
         props.bgColor === "purple" ? "#3C065E" : props.bgColor};
+        color: white;
+        display: flex;
+        justify-content: center;
+        position: relative;
+        padding-top: 8rem;
+        padding-bottom: 4rem;
+`
+
+export const StyledArticle = styled.article`
+    display: flex;
+    flex-direction: column;
+    width: 28rem;
+    margin: 1rem;
+    & > * {
+    margin: 1rem;
+  }
+`
+
+export const StyledIconContainer = styled.div`
+    display: flex;
+    font-size: 2rem;
+    & > * {
+    margin-right: 1rem;
+  }
+`
+
+export const StyledContactUs = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    position: absolute;
+    z-index: 1;
+    top: -7rem;
+    width: 100%;
+    @media (max-width: 1500px) {
+        top:-3.5rem;
+    }
+    & > * {
+    width: 27vmin;
+    height: 24vmin;
+    background-color: white;
+    box-shadow: rgb(0 0 0 / 15%) 0px 0px 10px 0px;
+    border-radius: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 5vmin;
+    color: #3C065E;
+    & > * {
+      margin: 1vmin;  
+    }
+  }
+`
+
+export const StyledH3 = styled.h3`
+    font-size: 3vmin;
+    color: black;
+    font-weight: normal;
+`
+
+export const StyledPara = styled.p`
+  font-size: 2vmin;
 `
