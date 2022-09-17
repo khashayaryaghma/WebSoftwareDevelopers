522c65a375e4290ba62c85da24baec60eb4cd2b8
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-17 13:06:18 +0430

    add article to SeventhSection

--- client/src/components/SeventhSection/SeventhSection.js
+++ client/src/components/SeventhSection/SeventhSection.js
@@ -1,8 +1,35 @@
-import { StyledSeventhSection } from "./SeventhSection.style"
+import { StyledSeventhSection, StyledArticle } from "./SeventhSection.style"
+import Button from "../Button/Button";
+import Image from "../Image/Image";
 
-function SeventhSection() {
+function SeventhSection({ bgColor }) {
   return (
-    <div>SeventhSection</div>
+    <StyledSeventhSection bgColor={bgColor}>
+      <StyledArticle>
+        <h1 className="heading">Web Analytics</h1>
+        <p className="para">
+          <em>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore</em>
+        </p>
+        <p className="para">
+          Image from
+          <a
+            href="https://www.freepik.com/vectors/computer"
+          >
+            {" "}
+            Freepik
+          </a>
+        </p>
+        <Button
+          bgColor="#29B7DD"
+          color="white"
+          brRadius="30px"
+          pd="0.5rem 3.2rem"
+          fSize="1.5rem"
+        >
+          R E A D M O R E
+        </Button>
+      </StyledArticle>
+    </StyledSeventhSection>
   )
 }
 
--- client/src/components/SeventhSection/SeventhSection.style.js
+++ client/src/components/SeventhSection/SeventhSection.style.js
@@ -4,3 +4,14 @@ export const StyledSeventhSection = styled.section`
     background-color: ${(props) =>
         props.bgColor === "purple" ? "#3C065E" : props.bgColor};
 `
+
+export const StyledArticle = styled.article`
+    width: 28rem;
+    min-height:26rem ;
+    padding: 3rem;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-around;
+`;
