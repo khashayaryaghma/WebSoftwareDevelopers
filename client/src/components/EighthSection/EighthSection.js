1ecf87c3ab20f7057316c27ae6b6d685889b5ddf
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-17 15:17:40 +0430

    add article and heading to EighthSection

--- client/src/components/EighthSection/EighthSection.js
+++ client/src/components/EighthSection/EighthSection.js
@@ -1,9 +1,29 @@
-import { StyledEighthSection } from "./EighthSection.style"
+import { StyledArticle, StyledEighthSection, StyledH2 } from "./EighthSection.style"
+import Button from "../Button/Button";
 
 function EighthSection() {
   return (
     <StyledEighthSection>
-      
+      <StyledH2>
+        We are directly involved in t​he process
+      </StyledH2>
+      <StyledArticle>
+        <p className="para" style={{color: "#808080"}}>
+          Nibh tortor id aliquet lectus. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. In hac habitasse platea dictumst quisque.
+        </p>
+        <p className="para">
+          Non quam lacus suspendisse faucibus interdum. Tempor nec feugiat nisl pretium. Mi in nulla posuere sollicitudin aliquam ultrices. Quisque egestas diam in arcu cursus euismod quis. Libero volutpat sed cras ornare arcu dui vivamus arcu.
+        </p>
+        <Button
+                    bgColor="#29B7DD"
+                    color="white"
+                    brRadius="30px"
+                    pd="0.5rem 3.2rem"
+                    fSize="1.2rem"
+                >
+                    L E A R N M O R E
+                </Button>
+      </StyledArticle>
     </StyledEighthSection>
   )
 }
--- client/src/components/EighthSection/EighthSection.style.js
+++ client/src/components/EighthSection/EighthSection.style.js
@@ -1,5 +1,37 @@
 import styled from "styled-components";
 
 export const StyledEighthSection = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: flex-start;
+    padding-top: 4rem;
+    padding-bottom: 4rem;
+`
 
+export const StyledH2 = styled.h2`
+    font-size: 50px;
+    width: 30rem;
+    padding: 2rem;
+    position: relative;
+    font-weight: normal;
+    &:before{
+            content: "";
+            position: absolute;
+            left: 2rem;
+            bottom: 0;
+            height: 5px;
+            width: 18%;
+            border-bottom: 5px solid #29B7DD;
+        }
+`
+
+export const StyledArticle = styled.article`
+    padding: 2rem;
+    display: flex;
+    width: 36rem;
+    min-height: 21rem;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
 `
