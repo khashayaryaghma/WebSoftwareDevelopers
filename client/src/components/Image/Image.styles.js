f801244b33343c6adb4a54639d5c0c84b9d5a0ed
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-18 17:36:55 +0430

    disable drag image

--- client/src/components/Image/Image.styles.js
+++ client/src/components/Image/Image.styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledImage = styled.img.attrs((props) => ({
     src: props.imgSrc,
     alt: props.alt,
+    draggable: false
 }))`
     width: ${(props) => props.size};
     border-radius: ${(props) => props.brRadius};
