f3fbe6a6c2e665c1d67e25f8849e04d938409a68
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-18 20:30:42 +0430

    complete SeventhSection

--- client/src/components/SeventhSection/SeventhSection.js
+++ client/src/components/SeventhSection/SeventhSection.js
@@ -1,33 +1,46 @@
-import { StyledSeventhSection, StyledArticle } from "./SeventhSection.style"
+import { StyledSeventhSection, StyledArticle, StyledContainerImage, StyledBBg, StyledContainerArticle, StyledSbgContainer } from "./SeventhSection.style"
 import Button from "../Button/Button";
+import Image from "../Image/Image";
+import seventhImage from "../../assets/images/seventhSection.jpg"
+import sbg from "../../assets/images/sbg.png"
 
 function SeventhSection({ bgColor }) {
   return (
     <StyledSeventhSection bgColor={bgColor}>
-      <StyledArticle>
-        <h2 className="heading">Web Analytics</h2>
-        <p className="para">
-          <em>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore</em>
-        </p>
-        <p className="para">
-          Image from
-          <a
-            href="https://www.freepik.com/vectors/computer"
+      <StyledContainerArticle>
+        <StyledSbgContainer>
+          <Image imgSrc={sbg} size="20vmax" />
+          <Image imgSrc={sbg} size="20vmax" />
+        </StyledSbgContainer>
+        <StyledArticle>
+          <h2 style={{ color: "#3C065E",fontSize:"2.5vmax" }}>Web Analytics</h2>
+          <p style={{fontSize:"1.1vmax"}}>
+            <em>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore</em>
+          </p>
+          <p style={{fontSize:"1.1vmax"}}>
+            Image from
+            <a
+              href="https://www.freepik.com/vectors/computer"
+            >
+              {" "}
+              Freepik
+            </a>
+          </p>
+          <Button
+            bgColor="#29B7DD"
+            color="white"
+            brRadius="30px"
+            pd="0.5vmax 3vmax"
+            fSize="0.8vmax"
           >
-            {" "}
-            Freepik
-          </a>
-        </p>
-        <Button
-          bgColor="#29B7DD"
-          color="white"
-          brRadius="30px"
-          pd="0.5rem 3.2rem"
-          fSize="1.2rem"
-        >
-          R E A D M O R E
-        </Button>
-      </StyledArticle>
+            R E A D M O R E
+          </Button>
+        </StyledArticle>
+      </StyledContainerArticle>
+      <StyledContainerImage>
+        <Image imgSrc={seventhImage} size="37vmax" />
+        <StyledBBg />
+      </StyledContainerImage>
     </StyledSeventhSection>
   )
 }
--- client/src/components/SeventhSection/SeventhSection.style.js
+++ client/src/components/SeventhSection/SeventhSection.style.js
@@ -3,18 +3,58 @@ import styled from "styled-components";
 export const StyledSeventhSection = styled.section`
     background-color: ${(props) =>
         props.bgColor === "purple" ? "#3C065E" : props.bgColor};
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 47vmax;
+        z-index: -4;
 `
 
 export const StyledArticle = styled.article`
-    max-width: 28rem;
-    min-height:26rem ;
-    padding: 3rem;
+    max-width: 24vmax;
+    padding: 2.2vmax;
+    padding-right:2.7vmax;
+    padding-left:2.7vmax ;
     background-color: white;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: space-around;
+    position: relative;
+    z-index:10;
     & > * {
-    margin: 1rem;
+    margin: 0.7vmax;
   }
 `;
+
+export const StyledContainerImage = styled.div`
+  position: relative;
+  z-index: 5;
+  top: -7vmax;
+  right: 5vmax;
+`
+
+export const StyledBBg = styled.div`
+  position: absolute;
+  z-index: -2;
+  background-color: #5D3BE7;
+  width: 37vmax;
+  height: 26vmax;
+  bottom: -2.75vmax;
+  right: -2.75vmax;
+`
+
+export const StyledContainerArticle = styled.div`
+  position: relative;
+  bottom: -4vmax;
+  right: -5vmax;
+`
+
+export const StyledSbgContainer = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  z-index: 5;
+  top: -4vmax;
+  right: -2.5vmax;
+`
