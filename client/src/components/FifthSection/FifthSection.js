967568f9498f1d8994b5d1263f9e149a43b33820
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-18 14:54:12 +0430

    complete FifthSection

--- client/src/components/FifthSection/FifthSection.js
+++ client/src/components/FifthSection/FifthSection.js
@@ -1,20 +1,21 @@
 import {
-    GridContainer,
-    StyledBox,
-    StyledDiv,
-    StyledFifthSection,
+    GridContainer, StyledBBgImage, StyledBgImage, StyledBox, StyledDiv, StyledFifthSection
 } from "./FifthSection.styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faComputer,
-    faGlobe,
-    faRobot,
-    faWrench,
-} from "@fortawesome/free-solid-svg-icons";
+import { faComputer, faGlobe, faRobot, faWrench } from "@fortawesome/free-solid-svg-icons";
+import Image from "../Image/Image";
+import fifthSectionImage from "../../assets/images/fifthSection.jpg"
+import sbg from "../../assets/images/sbg.png"
 
 function FifthSection({ bgColor }) {
     return (
         <StyledFifthSection bgColor={bgColor}>
+            <StyledBgImage>
+                <Image imgSrc={fifthSectionImage} size="49vmax"/>
+                <StyledBBgImage>
+                <Image imgSrc={sbg} size="26vmax"/>
+                <Image imgSrc={sbg} size="26vmax"/>
+                </StyledBBgImage>
             <GridContainer>
                 <StyledBox>
                     <StyledDiv>
@@ -41,6 +42,7 @@ function FifthSection({ bgColor }) {
                     <p style={{ fontSize: "1.2vmax" }}>AUTOMATION</p>
                 </StyledBox>
             </GridContainer>
+            </StyledBgImage>
         </StyledFifthSection>
     );
 }
--- client/src/components/FifthSection/FifthSection.styles.js
+++ client/src/components/FifthSection/FifthSection.styles.js
@@ -4,17 +4,28 @@ export const StyledFifthSection = styled.section`
     background-color: ${(props) =>
         props.bgColor === "purple" ? "#3C065E" : props.bgColor};
     display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 55vmax;
+    position:relative;
+    z-index: -2;
 `;
 
 export const GridContainer = styled.section`
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 1.2vmax;
+    position: absolute;
+    bottom: -5vmax;
+    right: -5vmax;
+    @media (max-width: 450px) {
+        right: -1vmax;
+    }
 `;
 export const StyledBox = styled.div`
     background-color: white;
-    width: 12vmax;
-    height: 8vmax;
+    width: 11vmax;
+    height: 7vmax;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -22,10 +33,27 @@ export const StyledBox = styled.div`
 `;
 
 export const StyledDiv = styled.div`
-    font-size: 1.5vmax;
+    font-size: 1.3vmax;
     border-radius: 50%;
-    margin-bottom: 1vmax;
-    padding: 0.5rem;
+    margin-bottom: 0.5vmax;
+    padding: 0.5vmax;
     background-color: #29b7dd;
     color: white;
 `;
+
+export const StyledBgImage = styled.div`
+    position: relative;
+    
+`
+
+export const StyledBBgImage = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: -2vmax;
+    right: -4vmax;
+    z-index: -1;
+    @media (max-width: 450px) {
+        right: -2vmax;
+    }
+`
