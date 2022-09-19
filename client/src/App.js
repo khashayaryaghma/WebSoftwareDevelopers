41871b4dd4f66dd67426916366a7a230aa4401ce
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-19 17:28:21 +0430

    add Login & remove Nav from App

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Contact from "./pages/Contact/Contact";
-import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
 import NotFound from "./pages/NotFound/NotFound";
 import Header from "./components/Header/Header";
 import logo from "./assets/images/logo.png";
-import NavBar from "./components/NavBar/NavBar";
-import { navItems, activeStyle } from "./constants/navItems";
 import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <div className="App">
             <Router>
-                <Header bgColor="purple" imgSrc={logo} pd="0.5rem" />
-                <NavBar
-                    bgColor="purple"
-                    navItems={navItems}
-                    activeStyle={activeStyle}
-                />
+                <Header bgColor="purple" imgSrc={logo} />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="contact" element={<Contact />} />
-                    <Route path="about" element={<About />} />
+                    <Route path="login" element={<Login />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
                 <Footer bgColor="purple" />
