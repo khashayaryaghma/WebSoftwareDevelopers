e2a49e50a19108ea5c131b6dbb59cb5b8e2df8a5
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-20 21:11:21 +0430

    set fixed position to Header

--- client/src/components/Header/Header.styles.js
+++ client/src/components/Header/Header.styles.js
@@ -4,6 +4,11 @@ export const StyledHeader = styled.header`
     background-color: ${(props) =>
         props.bgColor === "purple" ? "#3C065E" : props.bgColor};
     padding-top: 1rem;
+    position: fixed;
+    left: 0;
+    right: 0;
+    z-index: 20;
+    padding-bottom: 0.5rem;
     display: flex;
     justify-content: space-around;
     align-items: center;
