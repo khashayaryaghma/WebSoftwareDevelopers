489bafe26c41afb66ba17b6a8a1e593e0b974b85
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-20 16:50:29 +0430

    create Login form

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,8 +1,24 @@
-import React from 'react'
-
+import { StyledForm, StyledH1, StyledLogin } from "./Login.styles"
+import Input from "../../components/Input/Input"
+import Button from "../../components/Button/Button" 
 function Login() {
   return (
-    <div>Login</div>
+    <StyledLogin>
+      <StyledForm>
+        <StyledH1>Login</StyledH1>
+        <Input label="Username" type="text"/>
+        <Input label="Password" type="password"/>
+          <Button
+              bgColor="#29B7DD"
+              color="white"
+              brRadius="2px"
+              pd="0.5rem 3.2rem"
+              fSize="1.5rem"
+          >
+              L O G I N
+          </Button>
+      </StyledForm>
+    </StyledLogin>
   )
 }
 
---
+++ client/src/pages/Login/Login.styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledLogin = styled.section`
+    min-height: 53rem;
+    padding-bottom: 5vmax;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const StyledForm = styled.form`
+    position: absolute;
+    z-index: 10;
+    background-color: rgba(60, 6, 94, 0.85);
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    box-shadow: 0 15px 35px rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(15px);
+    color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 28rem;
+    height: 30rem;
+    border-radius: 7px;
+`
+
+export const StyledH1 = styled.h1`
+    font-size: 2rem;
+    margin-bottom: 3rem;
+`
