cf08139b0cb02e5f6c5a7742b02f9a97361218ff
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-09-27 16:46:51 +0330

    create simple notfoundpage

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,8 +1,13 @@
+import { Styled404 } from "./NotFound.styles"
 
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <Styled404>
+      <h1>
+        Page Not Found
+      </h1>
+    </Styled404>
   )
 }
 
---
+++ client/src/pages/NotFound/NotFound.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Styled404 = styled.section`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   font-size: 7vmax;
+   min-height: 50vmax;
+   padding: 8rem 2rem;
+`
